499b970210127033fae250b4dd7d361538e437a5
dqy <1016751306@qq.com>
2024-05-16 20:53:50 +0800

    feat: 增加玩家二游戏按钮控制

--- static/direction.js
+++ static/direction.js
@@ -33,3 +33,39 @@ document.getElementById('rightBtn').addEventListener('click', () => {
         directionQueue1.push({ dx: 1, dy: 0 });
     }
 });
+
+// w按钮
+document.getElementById('wBtn').addEventListener('click', () => {
+    if (directionQueue2.length === 0 && dy2 === 0) {
+        directionQueue2.push({ dx: 0, dy: -1 });
+    } else if (directionQueue2.length > 0 && directionQueue2[directionQueue2.length - 1].dy === 0) {
+        directionQueue2.push({ dx: 0, dy: -1 });
+    }
+});
+
+// s按钮
+document.getElementById('sBtn').addEventListener('click', () => {
+    if (directionQueue2.length === 0 && dy2 === 0) {
+        directionQueue2.push({ dx: 0, dy: 1 });
+    } else if (directionQueue2.length > 0 && directionQueue2[directionQueue2.length - 1].dy === 0) {
+        directionQueue2.push({ dx: 0, dy: 1 });
+    }
+});
+
+// a按钮
+document.getElementById('aBtn').addEventListener('click', () => {
+    if (directionQueue2.length === 0 && dx2 === 0) {
+        directionQueue2.push({ dx: -1, dy: 0 });
+    } else if (directionQueue2.length > 0 && directionQueue2[directionQueue2.length - 1].dx === 0) {
+        directionQueue2.push({ dx: -1, dy: 0 });
+    }
+});
+
+// d按钮
+document.getElementById('dBtn').addEventListener('click', () => {
+    if (directionQueue2.length === 0 && dx2 === 0) {
+        directionQueue2.push({ dx: 1, dy: 0 });
+    } else if (directionQueue2.length > 0 && directionQueue2[directionQueue2.length - 1].dx === 0) {
+        directionQueue2.push({ dx: 1, dy: 0 });
+    }
+});
