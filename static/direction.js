c40f81d9bff98988896bdc871b6f217b30097052
dqy <1016751306@qq.com>
2024-05-16 21:42:01 +0800

    fix: 修改方向按钮执行逻辑

--- static/direction.js
+++ static/direction.js
@@ -1,71 +1,149 @@
-// 上按钮
-document.getElementById('upBtn').addEventListener('click', () => {
-    if (directionQueue1.length === 0 && dy1 === 0) {
-        directionQueue1.push({ dx: 0, dy: -1 });
-    } else if (directionQueue1.length > 0 && directionQueue1[directionQueue1.length - 1].dy === 0) {
-        directionQueue1.push({ dx: 0, dy: -1 });
-    }
+const moveButtons = document.querySelectorAll('.move-btn');
+
+// 为每个移动按钮添加点击事件监听器
+moveButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.player === "1")
+            handlePlayer1Move(button.id);
+        else
+            handlePlayer2Move(button.id);
+    });
 });
 
-// 下按钮
-document.getElementById('downBtn').addEventListener('click', () => {
-    if (directionQueue1.length === 0 && dy1 === 0) {
-        directionQueue1.push({ dx: 0, dy: 1 });
-    } else if (directionQueue1.length > 0 && directionQueue1[directionQueue1.length - 1].dy === 0) {
-        directionQueue1.push({ dx: 0, dy: 1 });
+function handlePlayer1Move(id) {
+    switch (id) {
+        case "upBtn":
+            if (directionQueue1.length === 0 && dy1 === 0) {
+                directionQueue1.push({ dx: 0, dy: -1 });
+            } else if (directionQueue1.length > 0 && directionQueue1[directionQueue1.length - 1].dy === 0) {
+                directionQueue1.push({ dx: 0, dy: -1 });
+            }
+            break;
+        case "downBtn":
+            if (directionQueue1.length === 0 && dy1 === 0) {
+                directionQueue1.push({ dx: 0, dy: 1 });
+            } else if (directionQueue1.length > 0 && directionQueue1[directionQueue1.length - 1].dy === 0) {
+                directionQueue1.push({ dx: 0, dy: 1 });
+            }
+            break;
+        case "leftBtn":
+            if (directionQueue1.length === 0 && dx1 === 0) {
+                directionQueue1.push({ dx: -1, dy: 0 });
+            } else if (directionQueue1.length > 0 && directionQueue1[directionQueue1.length - 1].dx === 0) {
+                directionQueue1.push({ dx: -1, dy: 0 });
+            }
+            break;
+        case "rightBtn":
+            if (directionQueue1.length === 0 && dx1 === 0) {
+                directionQueue1.push({ dx: 1, dy: 0 });
+            } else if (directionQueue1.length > 0 && directionQueue1[directionQueue1.length - 1].dx === 0) {
+                directionQueue1.push({ dx: 1, dy: 0 });
+            }
+            break;
     }
-});
+}
 
-// 左按钮
-document.getElementById('leftBtn').addEventListener('click', () => {
-    if (directionQueue1.length === 0 && dx1 === 0) {
-        directionQueue1.push({ dx: -1, dy: 0 });
-    } else if (directionQueue1.length > 0 && directionQueue1[directionQueue1.length - 1].dx === 0) {
-        directionQueue1.push({ dx: -1, dy: 0 });
+function handlePlayer2Move(id) {
+    switch (id) {
+        case "wBtn":
+            if (directionQueue2.length === 0 && dy2 === 0) {
+                directionQueue2.push({ dx: 0, dy: -1 });
+            } else if (directionQueue2.length > 0 && directionQueue2[directionQueue2.length - 1].dy === 0) {
+                directionQueue2.push({ dx: 0, dy: -1 });
+            }
+            break;
+        case "sBtn":
+            if (directionQueue2.length === 0 && dy2 === 0) {
+                directionQueue2.push({ dx: 0, dy: 1 });
+            } else if (directionQueue2.length > 0 && directionQueue2[directionQueue2.length - 1].dy === 0) {
+                directionQueue2.push({ dx: 0, dy: 1 });
+            }
+            break;
+        case "aBtn":
+            if (directionQueue2.length === 0 && dx2 === 0) {
+                directionQueue2.push({ dx: -1, dy: 0 });
+            } else if (directionQueue2.length > 0 && directionQueue2[directionQueue2.length - 1].dx === 0) {
+                directionQueue2.push({ dx: -1, dy: 0 });
+            }
+            break;
+        case "dBtn":
+            if (directionQueue2.length === 0 && dx2 === 0) {
+                directionQueue2.push({ dx: 1, dy: 0 });
+            } else if (directionQueue2.length > 0 && directionQueue2[directionQueue2.length - 1].dx === 0) {
+                directionQueue2.push({ dx: 1, dy: 0 });
+            }
+            break;
     }
-});
+}
 
-// 右按钮
-document.getElementById('rightBtn').addEventListener('click', () => {
-    if (directionQueue1.length === 0 && dx1 === 0) {
-        directionQueue1.push({ dx: 1, dy: 0 });
-    } else if (directionQueue1.length > 0 && directionQueue1[directionQueue1.length - 1].dx === 0) {
-        directionQueue1.push({ dx: 1, dy: 0 });
-    }
-});
+// // 上按钮
+// document.getElementById('upBtn').addEventListener('click', () => {
+//     if (directionQueue1.length === 0 && dy1 === 0) {
+//         directionQueue1.push({ dx: 0, dy: -1 });
+//     } else if (directionQueue1.length > 0 && directionQueue1[directionQueue1.length - 1].dy === 0) {
+//         directionQueue1.push({ dx: 0, dy: -1 });
+//     }
+// });
 
-// w按钮
-document.getElementById('wBtn').addEventListener('click', () => {
-    if (directionQueue2.length === 0 && dy2 === 0) {
-        directionQueue2.push({ dx: 0, dy: -1 });
-    } else if (directionQueue2.length > 0 && directionQueue2[directionQueue2.length - 1].dy === 0) {
-        directionQueue2.push({ dx: 0, dy: -1 });
-    }
-});
+// // 下按钮
+// document.getElementById('downBtn').addEventListener('click', () => {
+//     if (directionQueue1.length === 0 && dy1 === 0) {
+//         directionQueue1.push({ dx: 0, dy: 1 });
+//     } else if (directionQueue1.length > 0 && directionQueue1[directionQueue1.length - 1].dy === 0) {
+//         directionQueue1.push({ dx: 0, dy: 1 });
+//     }
+// });
 
-// s按钮
-document.getElementById('sBtn').addEventListener('click', () => {
-    if (directionQueue2.length === 0 && dy2 === 0) {
-        directionQueue2.push({ dx: 0, dy: 1 });
-    } else if (directionQueue2.length > 0 && directionQueue2[directionQueue2.length - 1].dy === 0) {
-        directionQueue2.push({ dx: 0, dy: 1 });
-    }
-});
+// // 左按钮
+// document.getElementById('leftBtn').addEventListener('click', () => {
+//     if (directionQueue1.length === 0 && dx1 === 0) {
+//         directionQueue1.push({ dx: -1, dy: 0 });
+//     } else if (directionQueue1.length > 0 && directionQueue1[directionQueue1.length - 1].dx === 0) {
+//         directionQueue1.push({ dx: -1, dy: 0 });
+//     }
+// });
 
-// a按钮
-document.getElementById('aBtn').addEventListener('click', () => {
-    if (directionQueue2.length === 0 && dx2 === 0) {
-        directionQueue2.push({ dx: -1, dy: 0 });
-    } else if (directionQueue2.length > 0 && directionQueue2[directionQueue2.length - 1].dx === 0) {
-        directionQueue2.push({ dx: -1, dy: 0 });
-    }
-});
+// // 右按钮
+// document.getElementById('rightBtn').addEventListener('click', () => {
+//     if (directionQueue1.length === 0 && dx1 === 0) {
+//         directionQueue1.push({ dx: 1, dy: 0 });
+//     } else if (directionQueue1.length > 0 && directionQueue1[directionQueue1.length - 1].dx === 0) {
+//         directionQueue1.push({ dx: 1, dy: 0 });
+//     }
+// });
 
-// d按钮
-document.getElementById('dBtn').addEventListener('click', () => {
-    if (directionQueue2.length === 0 && dx2 === 0) {
-        directionQueue2.push({ dx: 1, dy: 0 });
-    } else if (directionQueue2.length > 0 && directionQueue2[directionQueue2.length - 1].dx === 0) {
-        directionQueue2.push({ dx: 1, dy: 0 });
-    }
-});
+// // w按钮
+// document.getElementById('wBtn').addEventListener('click', () => {
+//     if (directionQueue2.length === 0 && dy2 === 0) {
+//         directionQueue2.push({ dx: 0, dy: -1 });
+//     } else if (directionQueue2.length > 0 && directionQueue2[directionQueue2.length - 1].dy === 0) {
+//         directionQueue2.push({ dx: 0, dy: -1 });
+//     }
+// });
+
+// // s按钮
+// document.getElementById('sBtn').addEventListener('click', () => {
+//     if (directionQueue2.length === 0 && dy2 === 0) {
+//         directionQueue2.push({ dx: 0, dy: 1 });
+//     } else if (directionQueue2.length > 0 && directionQueue2[directionQueue2.length - 1].dy === 0) {
+//         directionQueue2.push({ dx: 0, dy: 1 });
+//     }
+// });
+
+// // a按钮
+// document.getElementById('aBtn').addEventListener('click', () => {
+//     if (directionQueue2.length === 0 && dx2 === 0) {
+//         directionQueue2.push({ dx: -1, dy: 0 });
+//     } else if (directionQueue2.length > 0 && directionQueue2[directionQueue2.length - 1].dx === 0) {
+//         directionQueue2.push({ dx: -1, dy: 0 });
+//     }
+// });
+
+// // d按钮
+// document.getElementById('dBtn').addEventListener('click', () => {
+//     if (directionQueue2.length === 0 && dx2 === 0) {
+//         directionQueue2.push({ dx: 1, dy: 0 });
+//     } else if (directionQueue2.length > 0 && directionQueue2[directionQueue2.length - 1].dx === 0) {
+//         directionQueue2.push({ dx: 1, dy: 0 });
+//     }
+// });
